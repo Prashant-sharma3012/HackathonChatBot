83d2b53807e58599e4d9d82126ebe5a43554757c
Kapil Pandey <41443779+kapi1Pandey@users.noreply.github.com>
2019-11-09 16:46:35 +0530

    Update Dashboard.js
    Added welcome ${userName} message in beginning of chat bot

--- frontEnd/src/components/dashboard/Dashboard.js
+++ frontEnd/src/components/dashboard/Dashboard.js
@@ -14,7 +14,7 @@ import './styles.css'
 
 
 class Dashboard extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       teamTitles: undefined,
@@ -24,14 +24,16 @@ class Dashboard extends Component {
     }
   }
   componentDidMount() {
-    addResponseMessage("Welcome to this awesome chat!");
-    axios.get('http://localhost:8000/teamInfo').then(res=>{
+    let { idToken } = JSON.parse(localStorage.getItem("okta-token-storage"))
+    let usrName = idToken.claims.name
+    addResponseMessage(`Welcome ${usrName}😊`);
+    axios.get('http://localhost:8000/teamInfo').then(res => {
       console.log("------------", res.data)
       const data = res.data.teamStructure
       const teamTitles = Object.keys(data)
       let count = 0
-      const positionTypeCount = teamTitles.map(d=>{
-        count = count+data[d].length;
+      const positionTypeCount = teamTitles.map(d => {
+        count = count + data[d].length;
         return data[d].length
       })
       console.log("=========teamTitles=========", teamTitles)
@@ -48,7 +50,7 @@ class Dashboard extends Component {
   handleNewUserMessage = (newMessage) => {
     console.log(`New message incomig! ${newMessage}`);
     // Now send the message throught the backend API
-    axios.post('http://localhost:8000/ansMsg', {msg: newMessage}).then(res=>{
+    axios.post('http://localhost:8000/ansMsg', { msg: newMessage }).then(res => {
       addResponseMessage(res.data.message)
     })
   }
@@ -64,124 +66,124 @@ class Dashboard extends Component {
     return (
       // <div style={{ height: "75vh" }} className="container valign-wrapper">
       <div>
-          <Grid
-            container
-            direction="row"
-            justify="flex-end"
-            alignItems="center"
+        <Grid
+          container
+          direction="row"
+          justify="flex-end"
+          alignItems="center"
+        >
+          <Fab
+            variant="extended"
+            aria-label="Apply"
+            title='Logout'
+            size="medium"
+            color="secondary"
+            onClick={this.onLogoutClick}
+            style={{
+              margin: 30
+            }}
           >
-            <Fab
-              variant="extended"
-              aria-label="Apply"
-              title='Logout'
-              size="medium"
-              color="secondary"
-              onClick={this.onLogoutClick}
-              style={{
-                margin: 30
-              }}
-            >
-              Logout&nbsp; <ExitToAppRoundedIcon />
-            </Fab>
-          </Grid>
-          <Grid xs={12}   container
-            direction="row"
-            justify="center"
-            alignItems="center">
+            Logout&nbsp; <ExitToAppRoundedIcon />
+          </Fab>
+        </Grid>
+        <Grid xs={12} container
+          direction="row"
+          justify="center"
+          alignItems="center">
           <Grid xs={5} item>
-          <Bar
-            height={300}
-             options= {{
-              title: {
-                display: true,
-                text: userInfo && `Team ${userInfo.account}`,
-                fontSize: 24
-              },
-              maintainAspectRatio: false,
-              responsive: true,
-              legend: {
-                display: false
-              },
-              animation: {
-                duration: 1000,
-                easing: "easeInOutQuint"
-              },
-              scales: {
-                xAxes: [
-                  {
-                    ticks: {
-                      autoSkip: false
-                    }
-                  }
-                ],
-                yAxes: [
-                  {
-                    ticks: {
-                      min: 0,
-                      stepSize: 2
-                    }
-                  }
-                ]
-              },
-              tooltips: {
-                callbacks: {
-                  label: toolTipItem => `Count: ${toolTipItem.yLabel}`
-                }
-              }
-            }}
-            data= {{
-              labels: teamTitles,
-              datasets: [{
-                barPercentage: 0.5,
-                label: 'Your Team',
-                data: positionTypeCount,
-                backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-              }]
-            }}
-              />
-
-          </Grid>
-            <Grid xs={5} item>
-            <Doughnut
-                data={{
-                  labels: teamTitles,
-                  datasets: [
+            <Bar
+              height={300}
+              options={{
+                title: {
+                  display: true,
+                  text: userInfo && `Team ${userInfo.account}`,
+                  fontSize: 24
+                },
+                maintainAspectRatio: false,
+                responsive: true,
+                legend: {
+                  display: false
+                },
+                animation: {
+                  duration: 1000,
+                  easing: "easeInOutQuint"
+                },
+                scales: {
+                  xAxes: [
                     {
-                      label: "% Share of members",
-                      backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850", ],
-                      data: positionTypeCount
+                      ticks: {
+                        autoSkip: false
+                      }
+                    }
+                  ],
+                  yAxes: [
+                    {
+                      ticks: {
+                        min: 0,
+                        stepSize: 2
+                      }
                     }
                   ]
-                }}
-                options={
+                },
+                tooltips: {
+                  callbacks: {
+                    label: toolTipItem => `Count: ${toolTipItem.yLabel}`
+                  }
+                }
+              }}
+              data={{
+                labels: teamTitles,
+                datasets: [{
+                  barPercentage: 0.5,
+                  label: 'Your Team',
+                  data: positionTypeCount,
+                  backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                }]
+              }}
+            />
+
+          </Grid>
+          <Grid xs={5} item>
+            <Doughnut
+              data={{
+                labels: teamTitles,
+                datasets: [
                   {
-                    title: {
-                      display: true,
-                      text: 'Percentage of different roles of people',
-                      fontSize: 20
-                    },
-                    tooltips: {
-                      callbacks: {
-                        label: (toolTipItem, data) => {
-                          // console.log(data.datasets[0].data[toolTipItem.index], totalCountOfTeam)
-                          const res = (data.datasets[0].data[toolTipItem.index]/totalCountOfTeam)*100
-                          return `${res.toFixed(2)} %`
-                        }
+                    label: "% Share of members",
+                    backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850",],
+                    data: positionTypeCount
+                  }
+                ]
+              }}
+              options={
+                {
+                  title: {
+                    display: true,
+                    text: 'Percentage of different roles of people',
+                    fontSize: 20
+                  },
+                  tooltips: {
+                    callbacks: {
+                      label: (toolTipItem, data) => {
+                        // console.log(data.datasets[0].data[toolTipItem.index], totalCountOfTeam)
+                        const res = (data.datasets[0].data[toolTipItem.index] / totalCountOfTeam) * 100
+                        return `${res.toFixed(2)} %`
                       }
                     }
                   }
                 }
-              />
-            </Grid>
-          </Grid>
-      
-           
-        <Widget
-              handleNewUserMessage={this.handleNewUserMessage}
-              profileAvatar={ShortLogo}
-              title="This is Bot baba reporting"
-              subtitle="At your service"
+              }
             />
+          </Grid>
+        </Grid>
+
+
+        <Widget
+          handleNewUserMessage={this.handleNewUserMessage}
+          profileAvatar={ShortLogo}
+          title="This is Bot baba reporting"
+          subtitle="At your service"
+        />
       </div>
     );
   }
